9e143953b37239452c4c086c0351992e9daa2fbf
John <johnfg2610@gmail.com>
2020-07-16 16:46:23 +0100

    changed path creation

--- middleware.go
+++ middleware.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"path"
 	"strings"
 
@@ -33,9 +34,9 @@ func NewJWTMiddleware(jwks *jose.JSONWebKeySet, issuer string) JWTMiddleware {
 	}
 }
 
-func NewJWTMiddlewareFromOpenID(baseURL string) (*JWTMiddleware, error) {
+func NewJWTMiddlewareFromOpenID(baseURL url.URL) (*JWTMiddleware, error) {
 
-	resp, err := http.Get(path.Join(baseURL, "/.well-known/openid-configuration"))
+	resp, err := http.Get(path.Join(baseURL.String(), "/.well-known/openid-configuration"))
 	if err != nil {
 		return nil, err
 	}

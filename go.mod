7d7f3dca1b52182202dc86095fc8be9b42cfd0f8
John <johnfg2610@gmail.com>
2020-07-19 21:42:45 +0100

    Finalized gojwt middleware setup

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/fleetgurux/gojwt
 go 1.14
 
 require (
-	github.com/pkg/errors v0.9.1
+	github.com/nats-io/jwt v1.0.1
 	github.com/square/go-jose v2.5.1+incompatible
-	github.com/square/go-jose/v3 v3.0.0-20200630053402-0a67ce9b0693 // indirect
+	github.com/square/go-jose/v3 v3.0.0-20200630053402-0a67ce9b0693
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 )
--- middleware.go
+++ middleware.go
@@ -8,30 +8,46 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
-	"github.com/square/go-jose"
+	"github.com/square/go-jose/v3"
+	"github.com/square/go-jose/v3/jwt"
 )
 
-type JWTError error
+//ContextKey used to access values in the context
 type ContextKey string
 
 const (
-	CONTEXT_JWT_ERROR  ContextKey = "JWT_ERROR"
-	CONTEXT_JWT_OBJECT ContextKey = "JWT_OBJECT"
+	//ContextJWTError is used to access the JWT error in the context
+	ContextJWTError ContextKey = "JWT_ERROR"
+	//ContextJWTObject is used to access the JWT object in the context
+	ContextJWTObject ContextKey = "JWT_OBJECT"
+	//ContextJWTClaims is used to access the JWT claims in the context
+	ContextJWTClaims ContextKey = "JWT_CLAIMS"
+	//ContextValidatorError is used to access the error provided by validators
+	ContextValidatorError ContextKey = "VALIDATOR_ERROR"
+
+	//ErrInvalidToken is returned as a error if the token was invalid
+	ErrInvalidToken = "The token provided was invalid"
 )
 
+//JWTMiddleware is used to mange JWT middleware
 type JWTMiddleware struct {
-	JWKS   *jose.JSONWebKeySet
-	Issuer string
+	JWKS        *jose.JSONWebKeySet
+	Issuer      string
+	EnableDebug bool
 }
 
+//NewJWTMiddleware is used to greate a new JWTMiddleware
 func NewJWTMiddleware(jwks *jose.JSONWebKeySet, issuer string) JWTMiddleware {
 	return JWTMiddleware{
-		JWKS:   jwks,
-		Issuer: issuer,
+		JWKS:        jwks,
+		Issuer:      issuer,
+		EnableDebug: false,
 	}
 }
 
+//NewJWTMiddlewareFromOpenID creates a new JWTMiddleware from a OpenID definition
 func NewJWTMiddlewareFromOpenID(baseURL string) (*JWTMiddleware, error) {
 
 	resp, err := http.Get(baseURL + "/.well-known/openid-configuration")
@@ -39,8 +55,8 @@ func NewJWTMiddlewareFromOpenID(baseURL string) (*JWTMiddleware, error) {
 		return nil, err
 	}
 
-	if resp.StatusCode < 199 && resp.StatusCode < 301 {
-		return nil, errors.New("Response code was not sucessfull")
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
 	}
 	byteArr, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -53,7 +69,7 @@ func NewJWTMiddlewareFromOpenID(baseURL string) (*JWTMiddleware, error) {
 		return nil, err
 	}
 
-	keyset, err := GetJWKSetFromOpenidURL(bodyWellKnown.JWKSURL)
+	keyset, err := GetJWKSetFromOpenIDURL(bodyWellKnown.JWKSURL)
 	if err != nil {
 		return nil, err
 	}
@@ -63,7 +79,8 @@ func NewJWTMiddlewareFromOpenID(baseURL string) (*JWTMiddleware, error) {
 
 }
 
-func GetJWKSetFromOpenidURL(JWKURL string) (*jose.JSONWebKeySet, error) {
+//GetJWKSetFromOpenIDURL creates a jsonwebkeyset from the JWK url provided
+func GetJWKSetFromOpenIDURL(JWKURL string) (*jose.JSONWebKeySet, error) {
 	resp, err := http.Get(JWKURL)
 	if err != nil {
 		return nil, err
@@ -85,37 +102,130 @@ func GetJWKSetFromOpenidURL(JWKURL string) (*jose.JSONWebKeySet, error) {
 	return &keySet, err
 }
 
-//TODO wrap errors
-func (mid *JWTMiddleware) JWTMiddleware(next http.Handler) http.Handler {
+//GetClaimsFromContext will get the claims from the context or return a error stating that it was unable to
+func GetClaimsFromContext(ctx context.Context) (*jwt.Claims, error) {
+	claims, exist := ctx.Value(ContextJWTClaims).(*jwt.Claims)
+	if !exist {
+		return nil, errors.New("JWT claims not found in context, Are you sure this was called after Middleware?")
+	}
+	return claims, nil
+}
+
+//GetTokenFromContext will get any tokens from context or return a error stating that it was unable to
+func GetTokenFromContext(ctx context.Context) (*jwt.JSONWebToken, error) {
+	jwt, exist := ctx.Value(ContextJWTObject).(*jwt.JSONWebToken)
+	if !exist {
+		return nil, errors.New("JWT object not found in context, Are you sure this was called after Middleware?")
+	}
+	return jwt, nil
+}
+
+//GetErrorFromContext will get any error from the current context or return a error stating that it was unable to
+func GetErrorFromContext(ctx context.Context) (error, error) {
+	err, exist := ctx.Value(ContextJWTError).(error)
+	if !exist {
+		return nil, errors.New("Error could not be found in context")
+	}
+	return err, nil
+}
+
+//GetValidatorErrorFromContext will get any validator error from the current context or return a error stating that it was unable to
+func GetValidatorErrorFromContext(ctx context.Context) (error, error) {
+	err, exist := ctx.Value(ContextValidatorError).(error)
+	if !exist {
+		return nil, errors.New("Validator error could not be found in context")
+	}
+	return err, nil
+}
+
+//Verifier validtes every request to test if it matches the provided JWKs
+func (mid *JWTMiddleware) Verifier(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		bearerHeaderRaw := r.Header.Get("Authorization")
-		fmt.Print(bearerHeaderRaw)
-		bearerHeader := strings.Replace(bearerHeaderRaw, "Bearer ", "", 0)
-		fmt.Print(bearerHeader)
-		fmt.Println(bearerHeader)
-		//tok, err := jwt.ParseSigned(bearerHeader)
+		if mid.EnableDebug {
+			fmt.Println("Raw bearer token: " + bearerHeaderRaw)
+		}
+		bearerHeader := strings.Replace(bearerHeaderRaw, "Bearer ", "", -1)
+		if mid.EnableDebug {
+			fmt.Println("Cleaned bearer token: " + bearerHeaderRaw)
+		}
 
-		object, err := jose.ParseSigned(bearerHeader)
+		tok, err := jwt.ParseSigned(bearerHeader)
+
+		var claims *jwt.Claims
 		if err != nil {
-			ctx := context.WithValue(r.Context(), CONTEXT_JWT_ERROR, err)
+			if mid.EnableDebug {
+				fmt.Println("Error getting claims: " + err.Error())
+			}
+			ctx := context.WithValue(r.Context(), ContextJWTError, err)
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 
-		//we have a valid key
-		var out interface{}
 		for _, jwk := range mid.JWKS.Keys {
-			out, err = object.Verify(jwk.Public())
-			if err != nil {
-				ctx := context.WithValue(r.Context(), CONTEXT_JWT_ERROR, err)
-				next.ServeHTTP(w, r.WithContext(ctx))
-				return
+			err = tok.Claims(jwk, &claims)
+
+			if err == nil {
+				break
 			}
-			break
 		}
 
-		ctx := context.WithValue(r.Context(), CONTEXT_JWT_OBJECT, out)
+		if claims == nil {
+			if mid.EnableDebug {
+				fmt.Println("Error no claims found: " + err.Error())
+			}
+			ctx := context.WithValue(r.Context(), ContextJWTError, err)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), ContextJWTObject, tok)
+		ctx = context.WithValue(r.Context(), ContextJWTClaims, claims)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+//ClaimsValidator will read claim values from context and validate, this includes ISS, EXP, NBF. if any validations fail a error will be logged under ContextValidatorError
+func (mid *JWTMiddleware) ClaimsValidator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		claims, err := GetClaimsFromContext(r.Context())
+		if err != nil {
+			if mid.EnableDebug {
+				fmt.Println("Error getting claims: " + err.Error())
+			}
+			ctx := context.WithValue(r.Context(), ContextValidatorError, err)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		err = claims.ValidateWithLeeway(jwt.Expected{
+			Issuer: mid.Issuer,
+		}, time.Second*30)
+
+		if err != nil {
+			if mid.EnableDebug {
+				fmt.Println("Error invalid token: " + err.Error())
+			}
+			ctx := context.WithValue(r.Context(), ContextValidatorError, err)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+//ClaimsTerminator will read any errors generated by claims validator and if any are found end the request with a `401` error code, this was seperated out to allow for more efficent logging to your chosen logging platform.
+func (mid *JWTMiddleware) ClaimsTerminator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err, _ := GetValidatorErrorFromContext(r.Context())
+
+		if err != nil {
+			fmt.Println("Error was not nil: " + err.Error())
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
 }
